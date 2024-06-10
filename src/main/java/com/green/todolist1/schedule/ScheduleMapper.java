f0e742b972c942c515ae2e3cf427908d2c1236a6
tg03067 <163490444+tg03067@users.noreply.github.com>
2024-06-10 18:11:45 +0900

    투두리스트asd

--- src/main/java/com/green/todolist1/schedule/ScheduleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.green.todolist1.schedule;
-
-import com.green.todolist1.schedule.model.SchedulePostReq;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ScheduleMapper {
-    int selSchedule(SchedulePostReq p);
-}
---
+++ src/main/java/com/green/todolist1/schedule/management/model/ScheduleManagementPostReq.java
@@ -0,0 +1,14 @@
+package com.green.todolist1.schedule.management.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class ScheduleManagementPostReq {
+    private long userSeq;
+    private long plantSeq;
+    private String managementDate;
+    @Schema(description = "1: 물주기, 2: 분갈이하기, 3: 가지치기, 4: 환기하기")
+    private int gardening;
+    private String contents;
+}
